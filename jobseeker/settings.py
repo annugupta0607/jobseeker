64a5a7dc0b5fbb18914f49ada787917cbeb62c77
Annu Gupta <annu.gupta0607@gmail.com>
2018-08-07 01:46:26 +0530

    debug true

--- jobseeker/settings.py
+++ jobseeker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uyq4e#he2v2xynupvdm3u4uwiqk+(k&gh@-p6=4*p$36ig7p6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
